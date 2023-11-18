16759cffdeb3bbcb58eadab57d23153a9fd1e70a
kjuhun <kjuhun00@ajou.ac.kr>
2023-11-18 22:09:06 +0900

    Add: comment

--- motor.h
+++ motor.h
@@ -4,13 +4,15 @@
 #include "header.h"
 #include "PWM.h"
 
-#define ENA 1 // ENA 핀
+//#define ENA 1 // ENA 핀
 #define IN1 4  // IN1 핀
 #define IN2 5  // IN2 핀
 
-#define ENB 26  // ENB 핀
+//#define ENB 26  // ENB 핀
 #define IN3 3 // IN3 핀
 #define IN4 2  // IN4 핀
+
+//ENA, ENB는 각각 gpio기준 18번,12번 핀에 꽂혀야함.
 
 #define PWM_RANGE 1024  // PWM 범위
 
